e96bf6c6ee32d04c2eb758d159c3c9788397032d
pei0804 <peeeei0804@gmail.com>
2018-09-15 15:17:00 +0900

    vs

--- vscode/extentionlist.sh
+++ vscode/extentionlist.sh
@@ -6,6 +6,13 @@
 
 pkglist=(
 negokaz.live-server-preview
+pkief.material-icon-theme
+shardulm94.trailing-spaces
+coenraads.bracket-pair-colorizer
+abusaidm.html-snippets
+ecmel.vscode-html-css
+zignd.html-css-class-completion
+kisstkondoros.vscode-gutter-preview
 )
 
 for i in ${pkglist[@]}; do
