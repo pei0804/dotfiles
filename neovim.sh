3fdf7cff66a912767b78d8273f21fa1c14d7412b
pei0804 <peeeei0804@gmail.com>
2018-12-11 00:36:21 +0900

    update

--- neovim.sh
+++ neovim.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-git clone https://github.com/powerline/fonts.git --depth=1\n
+git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
-./install.sh\n
-cd ..\n
-rm -rf fonts\n
+./install.sh
+cd ..
+rm -rf fonts
 
 git clone https://github.com/riywo/anyenv ~/.anyenv
 pyenv install 2.7.11
-pyenv install 3.4.4
+CFLAGS="-I$(brew --prefix readline)/include -I$(brew --prefix openssl)/include -I$(xcrun --show-sdk-path)/usr/include" \
+LDFLAGS="-L$(brew --prefix readline)/lib -L$(brew --prefix openssl)/lib" \
+PYTHON_CONFIGURE_OPTS=--enable-unicode=ucs2 \
+pyenv install -v 3.6.6
 pyenv virtualenv 2.7.11 neovim2
-pyenv virtualenv 3.4.4 neovim3
+pyenv virtualenv 3.6.6 neovim3
 pyenv activate neovim2
 pip install neovim
 pyenv activate neovim3
